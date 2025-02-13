14c5d0de9e8ff23df600e6e5911ba6ecf160f0d4
twofacefil11 <twofacefil11@gmail.com>
2025-02-13 04:20:46 +0100

    that was so stupid

--- configs/termux/fresh.sh
+++ configs/termux/fresh.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
-
-apt install wget
-apt install curl
-apt install neovim
-apt install zellij
-apt install yazi
-apt install rust
+echo "check if pkg update works first!"
+p
+pkg install git
+pkg install wget
+pkg install curl
+pkg install neovim
+pkg install zellij
+pkg install yazi
+pkg install rust
 
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+
+echo "Member to add lvim to path(its problematic)"A
+
+echo "also termux-reset settings or something"
+cp  .bashrc ~/
+cp -r .config ~/
+cp -r .termux ~/
+
+
